6eb4c1c246b00e51217af1fc235cc034beb6f6e9
louissarvin <louissarvin@users.noreply.github.com>
2026-05-28 14:00:00 +0700

    feat(contracts): IPausable external interface

---
+++ contracts/src/interfaces/external/IPausable.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.35;
+
+interface IPausable {
+    function pause() external;
+
+    function unpause() external;
+
+    function paused() external view returns (bool);
+}
